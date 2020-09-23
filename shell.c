a31e05fabd69dea5ef58026efd221553bf05a7f9
killjer215 <gerardo.prado01@utrgv.edu>
2020-09-22 22:46:54 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -77,7 +77,7 @@ int main(int argc, char **argv)
 	/* The Shell runs in an infinite loop, processing input. */
 
 		// TO-DO P5.2
-		fprintf(stdout, "Shell(pid=%d)%counter> ", shell_pid,counter);
+		fprintf(stdout, "Shell(pid=%d)%d> ", shell_pid,counter);
 		fflush(stdout);
 
 		/* Read a line of input. */
