e154ec1c699af0a1d294effa9ff120b914e4bb04
killjer215 <gerardo.prado01@utrgv.edu>
2020-09-23 02:28:20 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -170,7 +170,7 @@ int main(int argc, char **argv)
 		/* Execute Commands */
 			/* Try replacing 'fork()' with '0'.  What happens? */
 			pid_from_fork = fork();
-			printf("This is the fork number:%d \n\n",pid_from_fork);
+			//printf("This is the fork number:%d \n\n",pid_from_fork);
 			if (pid_from_fork < 0) {
 				/* Error: fork() failed.  Unlikely, but possible (e.g. OS *
 				 * kernel runs out of memory or process descriptors).     */
