91f87e28ecccbfd5e55054e2766c32fb1b64a2b9
killjer215 <gerardo.prado01@utrgv.edu>
2020-09-23 00:30:03 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -145,7 +145,7 @@ int main(int argc, char **argv)
 		
 		}
 		else {
-			if (exec_argv[0][0] == '!'){
+			/*if (exec_argv[0][0] == '!'){
 				int L = (int)(exec_argv[0][1])-48;
 				if(L < 1 || L > History_Counter)
 					fprintf(stderr, "Not valid\n%d",L);
@@ -153,7 +153,7 @@ int main(int argc, char **argv)
 					fprintf(stdout, "Its In the else if sojenfajsneoisadf\n%d%s",L,History[L-1]);
 					strcpy(exec_argv[0],History[L-1]);	
 				}
-			}
+			}*/
 		/* Execute Commands */
 			/* Try replacing 'fork()' with '0'.  What happens? */
 			pid_from_fork = fork();
